92ff840283bc07c3804ee214aa7a7401e7071c76
hwei0522 <75820302+hwei0522@users.noreply.github.com>
2024-05-01 18:44:22 +0800

    Add files via upload

---
+++ detection_model/pytorch_build_model.py
@@ -0,0 +1,86 @@
+import cv2
+import os
+import torch
+import time
+
+from .data_loader import get_transforms
+from .models import build_model
+from .post_processing import get_post_processing
+
+def resize_image(img, short_size):
+    height, width, _ = img.shape
+    if height < width:
+        new_height = short_size
+        new_width = new_height / height * width
+    else:
+        new_width = short_size
+        new_height = new_width / width * height
+    new_height = int(round(new_height / 32) * 32)
+    new_width = int(round(new_width / 32) * 32)
+    resized_img = cv2.resize(img, (new_width, new_height))
+    return resized_img
+
+
+class Pytorch_model:
+    def __init__(self, model_path, post_p_thre=0.7, gpu_id=None):
+        self.gpu_id = gpu_id
+
+        if self.gpu_id is not None and isinstance(self.gpu_id, int) and torch.cuda.is_available():
+            self.device = torch.device("cuda:%s" % self.gpu_id)
+        else:
+            self.device = torch.device("cpu")
+        print('device:', self.device)
+        checkpoint = torch.load(model_path, map_location=self.device)
+
+        config = checkpoint['config']
+        config['arch']['backbone']['pretrained'] = False
+        config['post_processing']['args']['box_thresh'] = post_p_thre
+        self.model = build_model(config['arch'])
+        self.post_process = get_post_processing(config['post_processing'])
+        self.post_process.box_thresh = post_p_thre
+        self.post_process.thresh = post_p_thre
+        self.img_mode = config['dataset']['train']['dataset']['args']['img_mode']
+        self.model.load_state_dict(checkpoint['state_dict'])
+        self.model.to(self.device)
+        self.model.eval()
+
+        self.transform = []
+        for t in config['dataset']['train']['dataset']['args']['transforms']:
+            if t['type'] in ['ToTensor', 'Normalize']:
+                self.transform.append(t)
+        self.transform = get_transforms(self.transform)
+
+    def predict(self, img_path: str, is_output_polygon=False, short_size: int = 1024):
+        assert os.path.exists(img_path), 'file is not exists'
+        img = cv2.imread(img_path, 1 if self.img_mode != 'GRAY' else 0)
+        if self.img_mode == 'RGB':
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        h, w = img.shape[:2]
+        img = resize_image(img, short_size)
+        # 将图片由(w,h)变为(1,img_channel,h,w)
+        tensor = self.transform(img)
+        tensor = tensor.unsqueeze_(0)
+
+        tensor = tensor.to(self.device)
+        batch = {'shape': [(h, w)]}
+        with torch.no_grad():
+            if str(self.device).__contains__('cuda'):
+                torch.cuda.synchronize(self.device)
+            start = time.time()
+            preds = self.model(tensor)
+            if str(self.device).__contains__('cuda'):
+                torch.cuda.synchronize(self.device)
+            box_list, score_list = self.post_process(batch, preds, is_output_polygon=is_output_polygon)
+            box_list, score_list = box_list[0], score_list[0]
+            if len(box_list) > 0:
+                if is_output_polygon:
+                    idx = [x.sum() > 0 for x in box_list]
+                    box_list = [box_list[i] for i, v in enumerate(idx) if v]
+                    score_list = [score_list[i] for i, v in enumerate(idx) if v]
+                else:
+                    idx = box_list.reshape(box_list.shape[0], -1).sum(axis=1) > 0  # 去掉全为0的框
+                    box_list, score_list = box_list[idx], score_list[idx]
+            else:
+                box_list, score_list = [], []
+            t = time.time() - start
+        return preds[0, 0, :, :].detach().cpu().numpy(), box_list, score_list, t
